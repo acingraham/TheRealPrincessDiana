c4737da8f8f3693320ae30250a4d45d3de89d2ee
Andrew Ingraham <aingraham@flatiron.com>
2019-05-01 00:53:01 -0400

    A bunch of updates

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const messages = randomize([
-	"First message",
-	"Second message",
-	"Third message",
+	{
+		text: "First message that is super super super super super super super super super super super super super super long",
+		author: "Person1",
+	},
+	{
+		text: "Second message that is super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super  super long",
+		author: "Person2",
+	},
+	{
+		text: "Third message",
+		author: "Person3",
+	},
 ]);
 
 const fadeLength = 1000;
@@ -10,14 +19,14 @@ const messageLength = 7000;
 const $h1 = $('h1');
 let index = 0;
 
-$h1.text(messages[index]);
+$h1.text(messages[index].text);
 $h1.fadeIn(fadeLength);
 
 setInterval(function() {
 	index = (index + 1) % messages.length;
 	const message = messages[index];
 	$h1.fadeOut(fadeLength, function() {
-		$h1.text(message);
+		$h1.text(message.text);
 	}).fadeIn(fadeLength);
 }, messageLength);
 
