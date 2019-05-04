ae2f27cb6e6c9c7971f0d1e2a1eeb093643a2fb1
Andrew Ingraham <aingraham@flatiron.com>
2019-05-04 01:59:34 -0400

    Support images and text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const messages = randomize([
+const testMessages = randomize([
 	{
 		text: "First message that is super super super super super super super super super super super super super super long",
 		author: "Person1",
@@ -13,20 +13,66 @@ const messages = randomize([
 	},
 ]);
 
+const messages = randomize([
+	{
+		type: 'pic',
+		name: 'bryan1.jpg'
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: 'Before I knew Diana, she picked up a package of mine accidentally and slacked me to get it from her desk. I went to retrieve it on the 5th floor, and thought she was very stylish in that Guns N Roses shirt. Quite the first impression.',
+		author: 'Bryan',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: 'How much time do you have? Quick wit comes to mind. Fierce loyalty. Pugnacious spirit if you dare wrong her. Diana is refreshing.',
+		author: 'Bryan',
+	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'Wry (in the best sense)',
+		author: 'Bryan',
+	},
+]);
+
+function renderPic(item) {
+	return `<img src="./images/${item.name}">`;
+}
+
+function renderText(item) {
+	return `
+		<h1>${item.prompt}</h1>
+		<hr>
+		<h1>${item.text}</h1>
+		<h1>- ${item.author}</h1>
+	`;
+}
+
+function render(item) {
+	if (item.type === 'pic') {
+		return renderPic(item);
+	} else {
+		return renderText(item);
+	}
+}
+
 const fadeLength = 1000;
 const messageLength = 7000;
 
-const $h1 = $('h1');
 let index = 0;
-
-$h1.text(messages[index].text);
-$h1.fadeIn(fadeLength);
+const $content = $('#content');
+const item = render(messages[index]);
+$content.html(item);
+$content.fadeIn(fadeLength);
 
 setInterval(function() {
 	index = (index + 1) % messages.length;
-	const message = messages[index];
-	$h1.fadeOut(fadeLength, function() {
-		$h1.text(message.text);
+	const item = render(messages[index]);
+	$content.fadeOut(fadeLength, function() {
+		$content.html(item);
 	}).fadeIn(fadeLength);
 }, messageLength);
 
