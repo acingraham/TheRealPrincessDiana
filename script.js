ec1ff5c5bf8ddc53e6b399567f67cac7e5d3b952
Andrew Ingraham <aingraham@flatiron.com>
2019-06-06 02:15:13 -0400

    More messages

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ const messages = randomize([
 		author: 'Thomas Marlette',
 	},
 	{
+		type: 'pic',
+		name: 'ahmadu1.jpg',
+		author: 'Ahmadu Gidado',
+	},
+	{
+		type: 'pic',
+		name: 'kim1.jpg',
+		author: 'Kim Shipman',
+	},
+	{
 		type: 'text',
 		prompt: 'Favorite memory of Diana...',
 		text: 'Before I knew Diana, she picked up a package of mine accidentally and slacked me to get it from her desk. I went to retrieve it on the 5th floor, and thought she was very stylish in that Guns N Roses shirt. Quite the first impression.',
@@ -90,6 +100,98 @@ const messages = randomize([
 		text: "melting into the rug with diana, tommy and andrew. connecting more strongly because we're better when we're together.",
 		author: 'Alda Pontes',
 	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'Caring',
+		author: 'Andrew Ingraham',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: "She has a positive impact on any relationship she's in.  She's incredibly invested in her friends and makes a real effort to improve others' lives.  I have a short list of absolutely amazing people that I've encountered and Diana made that list faster than anyone.  She's unreal.",
+		author: 'Andrew Ingraham',
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: "We were discussing ERGs (Employee Resource Groups) and Diana and her team had invested a lot of effort into creating bylaws.  During this informal meeting, we pushed back on a lot of the conclusions her team had reached.  I was absolutely blown away by Diana's receptiveness to feedback, even though it meant extra work for her and meant she'd need to push back against the group as a whole.  I was and still am so impressed by Diana's willingness to do what's right despite all the headaches it entails.  She's as good as they come.",
+		author: 'Andrew Ingraham',
+	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'gracious',
+		author: 'Ahmadu Gidado',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: "She cares deeply about the ones she loves",
+		author: 'Ahmadu Gidado',
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: "Hanging out with her family on the rooftop. It was lit.",
+		author: 'Ahmadu Gidado',
+	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'Genuine',
+		author: 'Kim Shipman',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: "How welcoming and inviting she is to everyone she meets",
+		author: 'Kim Shipman',
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: "In preparation for her 2018 Coachella trip, rapping old school Eminem lyrics in the recruiting pod",
+		author: 'Kim Shipman',
+	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'MAGNANIMOUS',
+		author: 'Misael Abreu',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: "Diana is all in with the people in her life - she doesn't just meet you, she digs deep!",
+		author: 'Misael Abreu',
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: "That one time that you hosted team trivia about team members at a team outing, revealing how you've been taking notes on the details of all our lives :)",
+		author: 'Misael Abreu',
+	},
+	{
+		type: 'text',
+		prompt: 'Describe Diana in one word...',
+		text: 'Sensational',
+		author: 'Kyle Konicek',
+	},
+	{
+		type: 'text',
+		prompt: 'What I love about Diana...',
+		text: "Her eye for art, ear for music, sense of style. Diana's genuine personality and overall positive vibe. Also her gutta gutta realness.",
+		author: 'Kyle Konicek',
+	},
+	{
+		type: 'text',
+		prompt: 'Favorite memory of Diana...',
+		text: `Anytime I would be having a wack work day, Diana would brighten it simply with a smile or a "whats up".`,
+		author: 'Kyle Konicek',
+	},
+
+
 ]);
 
 function renderPic(item) {
